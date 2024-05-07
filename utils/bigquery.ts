5f52c3968d775bbbfac09bc69c9866e7c0d3367a
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-05-07 11:39:31 -0700

    update to credentials parsing

--- utils/bigquery.ts
+++ utils/bigquery.ts
@@ -4,13 +4,15 @@ import { BigQuery } from '@google-cloud/bigquery';
 
 // Function to create a BigQuery client
 export function createBigQueryClient() {
-    const credentialsPath = process.env.BIGQUERY_CREDENTIALS_PATH;
-    if (!credentialsPath) {
+    const credentialsJSON = process.env.BIGQUERY_CREDENTIALS_PATH;
+    const credentials = JSON.parse(credentialsJSON);
+
+    if (!credentials) {
         throw new Error("The path to the BigQuery credentials is not set in the environment variables.");
     }
 
     const bigQueryClient = new BigQuery({
-        keyFilename: credentialsPath,
+        keyFilename: credentials,
     });
 
     return bigQueryClient;
