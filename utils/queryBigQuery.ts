f93db6caf77738df6b748fbd1e902b940fe980df
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-05-07 14:18:56 -0700

    added comments for clarity

--- utils/queryBigQuery.ts
+++ utils/queryBigQuery.ts
@@ -4,6 +4,7 @@ import { BigQuery } from '@google-cloud/bigquery';
 import { createBigQueryClient } from './bigquery';
 
 export async function queryBigQuery(sqlQuery: string): Promise<any[]> {
+    // init client and make request to google bigquery 
     const client = createBigQueryClient();
     const [job] = await client.createQueryJob({ query: sqlQuery });
     const [rows] = await job.getQueryResults();
