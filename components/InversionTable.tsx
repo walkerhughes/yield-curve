0814e244570ea0fef7c621a2566c10ecb2452b89
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-05-11 23:59:05 -0700

    minor edit to table formatting

--- components/InversionTable.tsx
+++ components/InversionTable.tsx
@@ -31,26 +31,26 @@ const InversionTable: React.FC = () => {
     if (!data) return <p>No data available.</p>;
 
     return (
-      <table style={tableStyle}>
+      <table>
           <thead>
               <tr>
-                  <th style={cellStyle}>Last Inversion Date</th>
-                  <th style={cellStyle}>Days Since Last Inversion</th>
-                  <th style={cellStyle}>Contango % 3M-10Y</th>
-                  <th style={cellStyle}>Difference 3M-10Y</th>
-                  <th style={cellStyle}>Contango % 2Y-10Y</th>
-                  <th style={cellStyle}>Difference 2Y-10Y</th>
+                  <th>Last Inversion Date</th>
+                  <th>Days Since Last Inversion</th>
+                  <th>Contango 3M-10Y</th>
+                  <th>Diff 3M-10Y</th>
+                  <th>Contango 2Y-10Y</th>
+                  <th>Diff 2Y-10Y</th>
               </tr>
           </thead>
           <tbody>
               {data.map((item, index) => (
                   <tr key={index}>
-                      <td style={cellStyle}>{item.last_inversion_date.value}</td>
-                      <td style={cellStyle}>{item.num_days_since_last_inversion}</td>
-                      <td style={cellStyle}>{item.contango_3m_10y}</td>
-                      <td style={cellStyle}>{item.diff_3m_10y}</td>
-                      <td style={cellStyle}>{item.contango_2y_10y}</td>
-                      <td style={cellStyle}>{item.diff_2y_10y}</td>
+                      <td>{item.last_inversion_date.value}</td>
+                      <td>{item.num_days_since_last_inversion}</td>
+                      <td>{item.contango_3m_10y}</td>
+                      <td>{item.diff_3m_10y}</td>
+                      <td>{item.contango_2y_10y}</td>
+                      <td>{item.diff_2y_10y}</td>
                   </tr>
               ))}
           </tbody>
