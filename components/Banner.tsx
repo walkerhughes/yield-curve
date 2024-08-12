5ecd56137cdafaff9d01f71f4b3a93e55ddf0a81
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-08-12 15:52:32 -0700

    minor edit to banner formatting in header

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -54,11 +54,7 @@ const TradingViewBanner = () => {
   return (
     <div className="tradingview-widget-container">
       <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener noreferrer" target="_blank">
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
+      <div className="tradingview-widget-copyright"></div>
     </div>
   );
 };
